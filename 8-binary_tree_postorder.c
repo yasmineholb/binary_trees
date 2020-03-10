f49f318c2506d440d7a42c0fb01e48cbb8b60497
yasmineholb <1055@holbertonschool.com>
2020-03-10 13:35:23 +0100

    postorder

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+*
+*
+*/
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+if (tree == NULL)
+return;
+if (func == NULL)
+return;
+else
+{
+binary_tree_postorder(tree->left, (*func));
+binary_tree_postorder(tree->right, (*func));
+func(tree->n);
+}
+}

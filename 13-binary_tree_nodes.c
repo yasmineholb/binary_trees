e0be2bb782beb32a53bab43072cc567ea9a8a5c2
Emna <bhmemna7@gmail.com>
2020-03-11 14:01:16 +0000

    betty fixing

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,14 +1,14 @@
 #include "binary_trees.h"
 /**
 * binary_tree_nodes - function that counts the nodes with at least 1 child
-* @tree - pointer to the given tree
+* @tree: pointer to the given tree
 * Return: the number of nodes with at least 1 child
 */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 if (!tree)
 return (0);
-if (tree-> left || tree->right)
+if (tree->left || tree->right)
 return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
 else
 return (0);
--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,7 +1,9 @@
 #include "binary_trees.h"
 /**
-*
-*
+* binary_tree_postorder - a function that goes through a binary
+* tree using post-order traversal
+* @tree:pointer
+* @func: function
 */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
